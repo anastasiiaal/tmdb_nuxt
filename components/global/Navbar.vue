6ddd71cd3eabc875716334d5c5642f9fbdcf0264
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-04-18 09:00:32 +0200

    better nav

--- components/global/Navbar.vue
+++ components/global/Navbar.vue
@@ -1,14 +1,17 @@
 <template>
-<ul>
-    <li>
-        <a href="/">Home</a>
-    </li>
-    <li>
-        <a href="/about">About</a>
-    </li>
-</ul>
+    <ul>
+        <li v-for="{ name, path } in menu" :key="link.path">
+            <a :href="path">{{ name }}</a>
+        </li>
+    </ul>
 </template>
 
 <script setup>
+
+    const menu = [
+        { name: 'Home', path: '/'},
+        { name: 'About', path: '/about'},
+        { name: 'Movies', path: '/movies'},
+    ]
 
 </script>
---
+++ pages/movies/index.vue
@@ -0,0 +1,11 @@
+<template>
+Movies index
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
